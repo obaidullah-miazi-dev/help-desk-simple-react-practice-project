61ebaa2b79d36dfca7f44c331430b1954b1d5cb7
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-24 18:16:11 +0600

    buttons toggling feature added

---
+++ src/Components/Btns.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Container from './Container';
+
+const Btns = ({toggle,setToggle}) => {
+    const btns = ['All', 'Pending', 'Submitted', 'Reviewed']
+
+    return (
+        <Container>
+            <div className='text-right'>
+                {
+                    btns.map((btn,ind) => {
+                        return (<button key={ind} className={`toggle-btn 
+                        ${ind == 0 && 'rounded-l-xl'} ${ind == btns.length-1 && 'rounded-r-xl'}
+                        ${toggle == btn && '!bg-purple-700 !text-white'}
+                        `} 
+                        onClick={() => setToggle(btn)}
+                        >
+                            {btn}
+                        </button>)
+
+                    })
+                }
+
+            </div>
+        </Container>
+    );
+};
+
+export default Btns;
--- src/Components/Issuemanage.jsx
+++ src/Components/Issuemanage.jsx
@@ -1,13 +1,16 @@
 import React, { use, useState } from 'react';
 import CountCards from './CountCards';
+import Btns from './Btns';
 
 const Issuemanage = ({ fetchPromise }) => {
     const initialData = use(fetchPromise)
     const [data, setData] = useState(initialData)
-    console.log(data);
+    const [toggle, setToggle] = useState('All')
+    
     return (
         <div>
             <CountCards data={data}></CountCards>
+            <Btns toggle={toggle} setToggle={setToggle}></Btns>
         </div>
     );
 };
