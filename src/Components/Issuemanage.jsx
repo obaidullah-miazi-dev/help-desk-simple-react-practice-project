28eafde2cd4a25c7d4d2bdc176d8775f9d4228a8
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-09-24 21:41:01 +0600

    issues status dynamically updating function added

--- src/Components/Issuemanage.jsx
+++ src/Components/Issuemanage.jsx
@@ -2,17 +2,28 @@ import React, { use, useState } from 'react';
 import CountCards from './CountCards';
 import Btns from './Btns';
 import IssuesCards from './IssuesCards';
+import Container from './Container';
 
 const Issuemanage = ({ fetchPromise }) => {
     const initialData = use(fetchPromise)
     const [data, setData] = useState(initialData)
     const [toggle, setToggle] = useState('All')
-    
+
+    const filteredData = toggle == 'All' ? data : data.filter((item) => item.status == toggle)
     return (
         <div>
             <CountCards data={data}></CountCards>
             <Btns toggle={toggle} setToggle={setToggle}></Btns>
-            <IssuesCards data={data} setData={setData}></IssuesCards>
+            <Container>
+                <div className='md:grid grid-cols-3 gap-5 pb-5 px-3 md:px-0 space-y-5 md:space-y-0'>
+                {
+                    filteredData.map((issue, ind) => {
+                        return <IssuesCards key={ind} data={data} issue={issue} setData={setData}></IssuesCards>
+                    })
+                }
+            </div>
+            </Container>
+
         </div>
     );
 };
--- src/Components/IssuesCards.jsx
+++ src/Components/IssuesCards.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import Container from './Container';
 
-const IssuesCards = ({ data, setData }) => {
-    console.log(data)
+const IssuesCards = ({ issue, data, setData }) => {
+    // console.log(issue)
+    const handleClick = () => {
+        const currentData = data.find((elm) => elm.ticketId == issue.ticketId)
+        const restData = data.filter((elm) => elm.ticketId !== issue.ticketId)
+        
+        if(currentData.status == 'Pending'){
+            currentData.status = 'Submitted'
+        } else if(currentData.status == 'Submitted'){
+            currentData.status = 'Reviewed'
+        }
+        
+        setData([currentData, ...restData])
+    }
     return (
         <Container>
-            <div className='md:grid grid-cols-3 gap-5 px-4 md:px-0 space-y-5 md:space-y-0'>
-                {
-                    data.map(element => <div className='mx-auto md:mx-0  bg-sky-50 shadow-md p-5 rounded-xl max-w-[550px] space-y-3'>
-                        <img className='w-20 h-20 rounded-full' src={element.userImg} alt="" />
-                        <h3 className='text-xl font-bold'>User: {element.requestedBy}</h3>
-                        <div className='md:flex space-y-2 md:space-y-0 justify-between items-center'>
-                            <p className='text-2xl font-bold'>{element.subject}</p>
-                            <div className='flex md:justify-between items-center gap-3'>
-                                <p className={` py-1 px-2 rounded-lg font-semibold
-                                ${element.priority == 'High' ? 'bg-red-100 text-red-500' : element.priority == 'Medium' ? 'text-yellow-500 bg-yellow-100' : 'text-green-600 bg-green-100'}
-                                `}>{element.priority}</p>
-                                <p className={`py-1 px-2 rounded-lg font-semibold
-                                ${element.status == 'Pending' ? 'bg-red-100 text-red-500' : element.status == 'Submitted' ? 'text-yellow-500 bg-yellow-100' : 'text-green-600 bg-green-100'}
-                                `}>{element.status}</p>
-                            </div>
-                        </div>
-                        <p>{element.description}</p>
-                    </div>)
-                }
+
+
+           
+             <div onClick={handleClick} className='mx-auto h-full md:mx-0 cursor-pointer bg-sky-50 shadow-md p-5 rounded-xl max-w-[550px] space-y-3'>
+                <img className='w-20 h-20 rounded-full' src={issue.userImg} alt="" />
+                <h3 className='text-xl font-bold'>User: {issue.requestedBy}</h3>
+                <div className='md:flex space-y-2 md:space-y-0 justify-between items-center'>
+                    <p className='text-2xl font-bold'>{issue.subject}</p>
+                    <div className='flex md:justify-between items-center gap-3'>
+                        <p className={` py-1 px-2 rounded-lg font-semibold
+                                ${issue.priority == 'High' ? 'bg-red-100 text-red-500' : issue.priority == 'Medium' ? 'text-yellow-500 bg-yellow-100' : 'text-green-600 bg-green-100'}
+                                `}>{issue.priority}</p>
+                        <p className={`py-1 px-2 rounded-lg font-semibold
+                                ${issue.status == 'Pending' ? 'bg-red-100 text-red-500' : issue.status == 'Submitted' ? 'text-yellow-500 bg-yellow-100' : 'text-green-600 bg-green-100'}
+                                `}>{issue.status}</p>
+                    </div>
+                </div>
+                <p>{issue.description}</p>
             </div>
+           
+
+
         </Container>
     );
 };
